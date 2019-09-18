91710e8b65fd20915117f2ca615b1c8dfdfe0e41
Tom852 <thomaskistler@bluewin.ch>
2019-09-18 13:54:39 +0200

    Hello World Demo Project
    for Proof of Work

---
+++ Dafny_Server_Redesign/Dafny_Server_Redesign/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dafny_Server_Redesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
