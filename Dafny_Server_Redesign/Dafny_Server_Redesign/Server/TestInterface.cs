43c376b8affbe715942286050fcb0bd054142826
just-me- <hess-marcel@bluewin.ch>
2019-10-07 16:26:21 +0200

    Cleanup ^^

--- Dafny_Server_Redesign/Dafny_Server_Redesign/Server/TestInterface.cs
+++ Dafny_Server_Redesign/Dafny_Server_Redesign/Server/TestInterface.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.Embedded.MediatR;
 using OmniSharp.Extensions.JsonRpc;
 
@@ -22,19 +21,13 @@ namespace Dafny_Server_Redesign.Server
 
     public class TestHandler : ITestInterface
     {
-        private readonly ILogger _logger;
-
-        public TestHandler(ILoggerFactory factory)
-        {
-            // _logger = factory.CreateLogger<ExpandAliasHandler>();
-        }
 
         public async Task<TestResult> Handle(TestParams request, CancellationToken cancellationToken)
         {
             // Do someting to get a return value 
             return new TestResult
             {
-                Text = "SomeResult" 
+                Text = "SomeResult Amumumuh" 
             };
         }
     }
