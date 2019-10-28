fb468340ec97320c705e58cae7147959e3fd00f0
Tom852 <38071521+Tom852@users.noreply.github.com>
2019-10-28 15:18:15 +0100

    verification service nochmal schnell angeschaut

--- dafny/Source/DafnyLanguageServer/VerificationService.cs
+++ dafny/Source/DafnyLanguageServer/VerificationService.cs
@@ -15,13 +15,31 @@ namespace DafnyLanguageServer
     {
         private static readonly int MAGICLINEENDING = 100; // 2Do evt dynamisch anpassen an jeweilige Zeilenlänge 
 
+
+        static public void Verify(ILanguageServer router, DafnyFile file)
+        {
+            // im Plugin das aktuelle Dokument setzen für die Statusbar
+            router.Window.SendNotification("activeVerifiyingDocument", file.Filepath);
+
+            var helper = DafnyVerify(file);
+            var diagnostics = CreateDafnyDiagnostics(helper, file);
+
+            PublishDiagnosticsParams p = new PublishDiagnosticsParams
+            {
+                Uri = file.Uri,
+                Diagnostics = new Container<Diagnostic>(diagnostics)
+            };
+            router.Document.PublishDiagnostics(p);
+            router.Window.SendNotification("updateStatusbar", diagnostics.Count);
+        }
+
         static private DafnyHelper DafnyVerify(DafnyFile file)
         {
             string[] args = new string[] { };
             DafnyHelper helper = new DafnyHelper(args, file.Filepath, file.Sourcecode);
             if (!helper.Verify())
             {
-                throw new ArgumentException("Failed to verify document.");
+                throw new ArgumentException("Failed to verify document."); //TODO: Während des schreibens ist das doc immer wieder invalid... exception ist etwas zu krass imho
             }
             return helper; 
         }
@@ -70,23 +88,6 @@ namespace DafnyLanguageServer
             }
 
             return diagnostics; 
-        }
-
-        static public void Verify(ILanguageServer router, DafnyFile file)
-        {
-            // im Plugin das aktuelle Dokument setzen für die Statusbar
-            router.Window.SendNotification("activeVerifiyingDocument", file.Filepath);
-
-            var helper = DafnyVerify(file);
-            var diagnostics = CreateDafnyDiagnostics(helper, file);
-            
-            PublishDiagnosticsParams p = new PublishDiagnosticsParams
-            {
-                Uri = file.Uri,
-                Diagnostics = new Container<Diagnostic>(diagnostics)
-            };
-            router.Document.PublishDiagnostics(p);
-            router.Window.SendNotification("updateStatusbar", diagnostics.Count);
         }
     }
 }
