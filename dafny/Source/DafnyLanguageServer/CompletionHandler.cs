5c16369cb2f30f354d39e8e80a2b680566728996
just-me- <hess-marcel@bluewin.ch>
2019-11-04 19:41:37 +0100

    Defaultwert. Schluckt er nun

--- dafny/Source/DafnyLanguageServer/CompletionHandler.cs
+++ dafny/Source/DafnyLanguageServer/CompletionHandler.cs
@@ -91,45 +91,14 @@ namespace DafnyLanguageServer
             var complitionItems = new List<CompletionItem>(); 
             foreach(var symbol in symbols)
             {
-                var kind = (CompletionItemKind)Enum.Parse(typeof(CompletionItemKind), symbol.SymbolType.ToString(), true);
+                CompletionItemKind kind = CompletionItemKind.Reference;
+                Enum.TryParse(symbol.SymbolType.ToString(), true, out kind);
 
                 complitionItems.Add(
                     new CompletionItem
                     {
                         Label = symbol.Name + " --- Type: " + symbol.SymbolType,
-                        //Kind = CompletionItemKind.Reference,
-                        Kind = (CompletionItemKind)Enum.Parse(typeof(CompletionItemKind), symbol.SymbolType.ToString(), true),
-                        //Kind = CompletionItemKind.Method,
-
-                        /* 
-                         *  {
-                        Text = 1,
-                        Method = 2,
-                        Function = 3,
-                        Constructor = 4,
-                        Field = 5,
-                        Variable = 6,
-                        Class = 7,
-                        Interface = 8,
-                        Module = 9,
-                        Property = 10,
-                        Unit = 11,
-                        Value = 12,
-                        Enum = 13,
-                        Keyword = 14,
-                        Snippet = 15,
-                        Color = 16,
-                        File = 17,
-                        Reference = 18,
-                        Folder = 19,
-                        EnumMember = 20,
-                        Constant = 21,
-                        Struct = 22,
-                        Event = 23,
-                        Operator = 24,
-                        TypeParameter = 25
-                    }
-                    */
+                        Kind = kind, 
                         TextEdit = new TextEdit
                         {
                             NewText = symbol.Name,
