3517787a713861f1ad4df3a51d95fd1a76dcb582
Tom852 <thomaskistler@bluewin.ch>
2019-11-11 20:41:01 +0100

    so ginge es mal mit pfad bei normalen beinarineries

--- dafny/Test/CounterExampleTest/CounterExampleTests.cs
+++ dafny/Test/CounterExampleTest/CounterExampleTests.cs
@@ -9,21 +9,17 @@ namespace Tests
     public class Tests
     {
         private static readonly string assemblyPath = Path.GetDirectoryName(typeof(Tests).Assembly.Location);
-        internal static readonly string testPath = Path.GetFullPath(Path.Combine(assemblyPath, "../CounterExampleFiles"));
+        internal static readonly string testPath = Path.GetFullPath(Path.Combine(assemblyPath, "../Test/CounterExampleFiles"));
 
         [Test]
         public void Fail1()
         {
             ExecutionEngine.printer = new DafnyConsolePrinter();
-            //string filename = Path.Combine(testPath, "fail.dfy");
+            string filename = Path.Combine(testPath, "fail.dfy");
 
-            //string source = File.ReadAllText(filename);
+            string source = File.ReadAllText(filename);
 
 
-            string filename = 
-                "d:\\Eigene Dokumente\\VisualStudio\\SA\\dafny - server - redesign\\VSCodePlugin\\counterExampleExampleFile.dfy";
-            string source =
-                "method MultipleReturns(inp1: int, inp2: int) returns (more: int, less: int)\r\n   ensures less < inp1 < more\r\n{\r\n   more := inp1 + inp2;\r\n   less := inp1 - inp2;\r\n}";
             var service = new CounterExampleService(filename, source);
             var results = service.ProvideCounterExamples().Result;
 
