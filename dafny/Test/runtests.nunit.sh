7e53e9515879a05cf56d32bd89a1c4d75b9b295a
Tom852 <thomaskistler@bluewin.ch>
2019-11-11 17:51:00 +0100

    scriptly -- verification service in progress

---
+++ dafny/Test/runtests.nunit.sh
@@ -0,0 +1,3 @@
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CompileHandlerTest.dll
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe dafny/Test/CompletionHandlerTest.dll
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe dafny/Test/VerificationServiceTest.dll
