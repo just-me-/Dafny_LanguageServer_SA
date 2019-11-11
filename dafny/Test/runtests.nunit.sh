6d8fd8190522b1fd8c0ef9e5d10c91e80441adea
Tom852 <thomaskistler@bluewin.ch>
2019-11-11 18:09:45 +0100

    dann doch kein script

--- dafny/Test/runtests.nunit.sh
+++
@@ -1,4 +0,0 @@
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CompileHandlerTest.dll
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CompletionHandlerTest.dll
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/VerificationServiceTest.dll
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CounterExampleTest.dll
