cc925a380092445740a817ff7e656b9b9c5a5e51
Tom852 <thomaskistler@bluewin.ch>
2019-11-11 18:04:08 +0100

    so probieren

--- dafny/Test/runtests.nunit.sh
+++ dafny/Test/runtests.nunit.sh
@@ -1,3 +1,4 @@
 mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CompileHandlerTest.dll
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe dafny/Test/CompletionHandlerTest.dll
-mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe dafny/Test/VerificationServiceTest.dll
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CompletionHandlerTest.dll
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/VerificationServiceTest.dll
+mono ./NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe TestBinaries/CounterExampleTest.dll
