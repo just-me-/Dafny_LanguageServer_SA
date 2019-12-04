03dbb4a7f77e2b29c2b9e5bdd71613c1279d35b5
Tom852 <thomaskistler@bluewin.ch>
2019-12-04 16:49:59 +0100

    added test for counter example with fake translation unit

--- dafny/Test/CounterExampleTest/CounterExampleTests.cs
+++ dafny/Test/CounterExampleTest/CounterExampleTests.cs
@@ -1,17 +1,105 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using DafnyLanguageServer;
 using DafnyLanguageServer.DafnyAdapter;
 using Microsoft.Boogie;
 using Microsoft.Dafny;
 using NUnit.Framework;
+using CounterExamples = System.Collections.Generic.List<DafnyLanguageServer.DafnyAdapter.CounterExampleProvider.CounterExample>;
 using DafnyConsolePrinter = DafnyLanguageServer.DafnyAdapter.DafnyConsolePrinter;
 
 namespace Tests
 {
-    public class Tests
+
+    class FakeTUForCounterExamples : IDafnyTranslationUnit
     {
-        private static readonly string assemblyPath = Path.GetDirectoryName(typeof(Tests).Assembly.Location);
+        private CounterExamples counterExamples;
+        public FakeTUForCounterExamples(CounterExamples counterExamples) => this.counterExamples = counterExamples;
+        public FakeTUForCounterExamples(int col, int row, string[] vars, string[] vals)
+        {
+            CounterExamples fakeData = new CounterExamples();
+            fakeData.Add(new CounterExampleProvider.CounterExample());
+            var states = fakeData[0].States;
+            //first two states are ignored
+            states.Add(new CounterExampleProvider.CounterExampleState());
+            states.Add(new CounterExampleProvider.CounterExampleState());
+
+
+            states.Add(new CounterExampleProvider.CounterExampleState()); //Option: Support multiple states. Statt im CTOR ne methode machen "add CE mti diesen params"
+            var state = fakeData[0].States[2];
+            state.Column = col;
+            state.Line = row;
+            state.Variables = new List<CounterExampleProvider.CounterExampleVariable>();
+
+            for (int i = 0; i < vars.Length; i++)
+            {
+                state.Variables.Add(new CounterExampleProvider.CounterExampleVariable
+                {
+                    //CanonicalName = "a",
+                    Name = vars[i],
+                    //RealName = "c",
+                    Value = vals[i]
+                });
+            }
+
+            counterExamples = fakeData;
+        }
+
+        public CounterExamples CounterExample() => counterExamples;
+
+        public bool Verify() { throw new NotImplementedException(); }
+        public List<ErrorInformation> GetErrors() { throw new NotImplementedException(); }
+        public List<SymbolTable.SymbolInformation> Symbols() { throw new NotImplementedException(); }
+        public void DotGraph() { throw new NotImplementedException();}
+    } 
+
+    public class UnitTests
+    {
+
+
+        [Test]
+        public void FakeDataTestYaaay()
+        {
+            int col = 2;
+            int row = 2;
+            string[] vars = {"myVar"};
+            string[] vals = { "myVal" };
+
+            FakeTUForCounterExamples fakeTU = new FakeTUForCounterExamples(col, row, vars, vals);
+
+            var service = new CounterExampleService(fakeTU);
+            CounterExampleResults results = service.ProvideCounterExamples().Result;
+
+            Assert.AreEqual(1, results.CounterExamples.Count);
+
+            foreach (CounterExampleResult r in results.CounterExamples)
+            {
+                Assert.AreEqual(col, r.Col);
+                Assert.AreEqual(row, r.Line);
+
+
+                foreach (string var in vars)
+                {
+                    Assert.Contains(var, r.Variables.Keys);
+                }
+
+                for (int i = 0; i < r.Variables.Count; i++)
+                {
+                    Assert.AreEqual(vars[i], r.Variables.Keys.ToList()[i]);
+                    Assert.AreEqual(vals[i], r.Variables[vars[i]]);
+                }
+
+            }
+        }
+
+
+
+    }
+    public class IntegrationTests
+    {
+        private static readonly string assemblyPath = Path.GetDirectoryName(typeof(IntegrationTests).Assembly.Location);
         internal static readonly string testPath = Path.GetFullPath(Path.Combine(assemblyPath, "../Test/CounterExampleFiles"));
         
         private CounterExampleResults ProvideCounterExamples(string filename)
