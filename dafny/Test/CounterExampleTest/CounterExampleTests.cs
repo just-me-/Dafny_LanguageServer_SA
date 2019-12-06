e511575d929a7a05a97709121ec390c8c1ecd2b5
Tom852 <thomaskistler@bluewin.ch>
2019-12-06 12:35:21 +0100

    added more CounterExampe Unit Tests

--- dafny/Test/CounterExampleTest/CounterExampleTests.cs
+++ dafny/Test/CounterExampleTest/CounterExampleTests.cs
@@ -62,6 +62,20 @@ namespace Tests
     public class UnitTests
     {
         [Test]
+        public void NoCounterExample()
+        {
+
+            FakeDafnyTranslationUnitForCounterExamples fake = new FakeDafnyTranslationUnitForCounterExamples();
+
+            var service = new CounterExampleService(fake);
+            CounterExampleResults results = service.ProvideCounterExamples().Result;
+
+            Assert.AreEqual(0, results.CounterExamples.Count,
+                $"Counter Example should only contain 0 counter examples.");
+
+        }
+
+        [Test]
         public void SingleCounterExample()
         {
             const int col = 2;
@@ -83,7 +97,27 @@ namespace Tests
 
 
         [Test]
-        public void TwoCounterExample()
+        public void SingleCounterExampleWithTwoVars()
+        {
+            const int col = 2;
+            const int row = 2;
+            string[] vars = { "myVar", "otherVar" };
+            string[] vals = { "myVal", "otherVal" };
+
+            FakeDafnyTranslationUnitForCounterExamples fake = new FakeDafnyTranslationUnitForCounterExamples();
+            fake.AddCounterExampleToFake(col, row, vars, vals);
+
+            var service = new CounterExampleService(fake);
+            CounterExampleResults results = service.ProvideCounterExamples().Result;
+
+            Assert.AreEqual(1, results.CounterExamples.Count,
+                $"Counter Example should only contain 1 counter examples.");
+
+            CompareCounterExampleWithExpectation(results.CounterExamples[0], col, row, vars, vals);
+        }
+
+        [Test]
+        public void TwoCounterExamples()
         {
             const int col1 = 2;
             const int row1 = 2;
@@ -110,6 +144,44 @@ namespace Tests
             CompareCounterExampleWithExpectation(results.CounterExamples[1], col2, row2, vars2, vals2);
         }
 
+
+        [Test]
+        public void ManyExamplesWithManyVars()
+        {
+            const int col1 = 1;
+            const int row1 = 31;
+            string[] vars1 = { "myVar", "a", "b" };
+            string[] vals1 = { "myVal", "-2", "((3))" };
+
+
+            const int col2 = 211;
+            const int row2 = 2123;
+            string[] vars2 = { "abc", "def", "ghi" };
+            string[] vals2 = { "0", "((-23))", "#less'1" };
+
+
+            const int col3 = 22;
+            const int row3 = 222;
+            string[] vars3 = { "abc" };
+            string[] vals3 = { "#less'1" };
+
+            FakeDafnyTranslationUnitForCounterExamples fake = new FakeDafnyTranslationUnitForCounterExamples();
+            fake.AddCounterExampleToFake(col1, row1, vars1, vals1);
+            fake.AddCounterExampleToFake(col2, row2, vars2, vals2);
+            fake.AddCounterExampleToFake(col3, row3, vars3, vals3);
+
+            var service = new CounterExampleService(fake);
+            CounterExampleResults results = service.ProvideCounterExamples().Result;
+
+            Assert.AreEqual(3, results.CounterExamples.Count,
+                $"Counter Example should only contain 3 counter examples.");
+
+            CompareCounterExampleWithExpectation(results.CounterExamples[0], col1, row1, vars1, vals1);
+            CompareCounterExampleWithExpectation(results.CounterExamples[1], col2, row2, vars2, vals2);
+            CompareCounterExampleWithExpectation(results.CounterExamples[2], col3, row3, vars3, vals3);
+        }
+
+
         private static void CompareCounterExampleWithExpectation(CounterExampleResult r, int col, int row, string[] vars, string[] vals)
         {
             Assert.AreEqual(col, r.Col,
@@ -135,7 +207,9 @@ namespace Tests
                 "The Key-Value-Pairs 'variable: value' are not equal"
             );
 
+            var a = r.Variables.OrderBy(kv => kv.Key).ToList();
         }
+
     }
     public class IntegrationTests
     {
